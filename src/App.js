d5d0c9118c234bc7c8f274a2a9c86a0bcd2815d8
Zoi Pistioli <zoi.pistioli@gmail.com>
2020-12-05 12:06:54 +0200

    basic implementation of navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      <h1>Codehub React App</h1>
+      <Header/>
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
+
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Code.Hub Dashboard</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="">Courses</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="">Add new course</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
