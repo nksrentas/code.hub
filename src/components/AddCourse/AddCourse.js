4f9cc456765d5588c6b70fc5d47ef8a0ac2a3f5e
Zoi Pistioli <zoi.pistioli@gmail.com>
2020-12-20 23:21:47 +0200

    reload course details page after edit

--- src/components/AddCourse/AddCourse.js
+++ src/components/AddCourse/AddCourse.js
@@ -38,7 +38,8 @@ const AddCourse = forwardRef((props, ref) => {
         .then((res) => {
           console.log('Post response', res);
           toggle();
-          history.push('/courses');
+          window.location.reload(false);
+          //history.push('/courses');
         })
         .catch((err) => {
           console.error(err);
